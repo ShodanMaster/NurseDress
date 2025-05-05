edae7a2eb7ed72723003d40c02b652c8d2a02461
ShodanMaster <akshaykrishnan922@gmail.com>
2025-05-05 23:28:58 +0530

    Size Excel Export

--- app/Exports/SizesExport.php
+++ app/Exports/SizesExport.php
@@ -4,15 +4,51 @@ namespace App\Exports;
 
 use App\Models\Size;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\WithHeadings;
+use Maatwebsite\Excel\Concerns\WithMapping;
+use Maatwebsite\Excel\Concerns\WithStyles;
+use PhpOffice\PhpSpreadsheet\Worksheet\Worksheet;
 
-class SizesExport implements FromCollection
+class SizesExport implements FromCollection, WithHeadings, WithMapping, WithStyles
 {
     /**
-    * @return \Illuminate\Support\Collection
-    */
-    
+     * @return \Illuminate\Support\Collection
+     */
     public function collection()
     {
         return Size::all();
+    }
+
+    /**
+     * Map each row of data
+     */
+    public function map($size): array
+    {
+        static $index = 1;
+        return [
+            $index++,
+            $size->name,
+        ];
+    }
+
+    /**
+     * Define custom headings
+     */
+    public function headings(): array
+    {
+        return [
+            'S.No',
+            'Name',
+        ];
+    }
+
+    /**
+     * Apply styles to the sheet
+     */
+    public function styles(Worksheet $sheet)
+    {
+        return [
+            1 => ['font' => ['bold' => true]],
+        ];
     }
 }
